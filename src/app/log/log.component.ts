400e16ec12d704fcd748c7c854e20a2c8775ef89
kj <msrndsy@gmail.com>
2022-11-22 05:05:59 +0900

    initial commit

---
+++ src/app/log/log.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { LogService } from '../log.service';
+
+@Component({
+  selector: 'app-log',
+  templateUrl: './log.component.html',
+  styleUrls: ['./log.component.scss']
+})
+export class LogComponent implements OnInit {
+  @Input() log : string = '';
+  @ViewChild('logtextarea') logtextarea : ElementRef | undefined;
+
+  autoscroll : boolean = true;
+  logboxScrollHeight : number = 0;
+  
+  autoscroll_toggle(): void {
+    this.autoscroll = this.autoscroll ? false : true;
+    console.log('autoscroll ' + this.autoscroll)
+  }
+
+  clearlog() : void {
+    this.log = '';
+  }
+
+  constructor(public logservice: LogService) { 
+    logservice.log_subject.subscribe(
+      {next : (msg) => 
+        {
+          this.log += msg + '\n'
+          
+          if (this.autoscroll){
+            var logarea : any = this.logtextarea?.nativeElement;
+            if (logarea != undefined)
+              logarea.scrollTop = logarea.scrollHeight;
+          }
+        }
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
